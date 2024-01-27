d853e89de17cb90563f1a8e1fedad1a611475b56
ArthurAb <abgaryanarthurelina@gmail.com>
2024-01-27 12:35:53 +0300

    модель image

---
+++ image_socal/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+
+#Модель для сохранения изображения
+class Image(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'user_image')
+    description = models.CharField(max_length = 100, blank=True)
+    date_created = models.DateField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length =100)
+    slug = models.SlugField(blank=True, max_length=100)
+    url = models.URLField()
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    users_likes = models.ManyToManyField(User, related_name='images_like', blank=True)
+
+    def __str__(self):
+        return self.title
+#автоматичуское заполнение поля slug по полю title
+    def save(self,*args, **kwargs):
+        self.slug = slugify(self.title)
+        return super(Image,self).save(*args, **kwargs)
+
+# Create your models here.
